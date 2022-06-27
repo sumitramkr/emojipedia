6d60ae358cf75adab060a611bbd745aa1b27b4db
sumitramkr <sumitramkr77e@gmail.com>
2022-06-27 23:43:55 +0530

    added arrow function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import Entry from "./Entry";
 import emojipedia from "../emojipedia";
 
-function createEmoji(emojipedia) {
-  return (
-    <Entry
-      key={emojipedia.id}
-      emojiEntry={emojipedia.emoji}
-      nameEntry={emojipedia.name}
-      meaningEntry={emojipedia.meaning}
-    />
-  );
-}
+// function createEmoji(emojipedia) {
+//   return (
+//     <Entry
+//       key={emojipedia.id}
+//       emojiEntry={emojipedia.emoji}
+//       nameEntry={emojipedia.name}
+//       meaningEntry={emojipedia.meaning}
+//     />
+//   );
+// }
 
 function App() {
   return (
@@ -19,7 +19,16 @@ function App() {
       <h1>
         <span>emojipedia</span>
       </h1>
-      <dl className="dictionary">{emojipedia.map(createEmoji)}</dl>
+      <dl className="dictionary">
+        {emojipedia.map((emojipedia) => (
+          <Entry
+            key={emojipedia.id}
+            emojiEntry={emojipedia.emoji}
+            nameEntry={emojipedia.name}
+            meaningEntry={emojipedia.meaning}
+          />
+        ))}
+      </dl>
     </div>
   );
 }
